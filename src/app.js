d5902157dd137e0d11dceee193d01a32a5234363
Michell D'Enjoy <166136472+michelldenjoy@users.noreply.github.com>
2024-06-24 23:38:16 +0000

    Excuse generator by michell

--- src/app.js
+++ src/app.js
@@ -8,4 +8,30 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ["Cleopatra", "Superwoman", "Xena", "Princess Mononoke"];
+  let action = ["attacked", "hit", "killed", "fight with"];
+  let what = ["the enemy", "the Joker", "Freddy Krueguer", "Voldemort"];
+  let when = ["last night", "this morning", "yesterday", "when i was working"];
+  let and = ["and i had to help her"];
+
+  function randomgenerador(listadepalabras) {
+    const palabra =
+      listadepalabras[Math.floor(Math.random() * listadepalabras.length)];
+
+    return palabra;
+  }
+
+  const randomExcuse = () =>
+    `${randomgenerador(who)} ${randomgenerador(action)} ${randomgenerador(
+      what
+    )} ${randomgenerador(when)} ${randomgenerador(and)}`;
+  console.log(randomExcuse());
+
+  function mostrarExcusa() {
+    document.getElementById("generador").innerHTML = randomExcuse();
+  }
+
+  mostrarExcusa();
+  document.getElementById("boton").addEventListener("click", mostrarExcusa);
 };
